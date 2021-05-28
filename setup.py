e2baad860a26b55cc879d523ba6dee84b50a45ec
chris-belcher <chris-belcher@users.noreply.github.com>
2021-05-28 23:40:20 +0100

    Update version and release notes for 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="electrum-personal-server",
-    version="0.2.1.1",
+    version="0.2.2",
     description="Electrum Personal Server",
     author="Chris Belcher",
     license="MIT",
